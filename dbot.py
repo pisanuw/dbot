61a773ec9192e6b44c83d4f5c500320aa52edd18
Yusuf Pisan <pisan@uw.edu>
2021-06-18 04:58:30 -0700

    start

---
+++ dbot.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import configparser
+import discord
+
+USER_INI = os.path.abspath(os.path.expanduser('~/.dbot'))
+
+config = configparser.ConfigParser(allow_no_value=True)
+config.read(USER_INI)
+
+BOT_TOKEN = config['DEFAULT']['BOT_TOKEN']
+BOT_USER = config['DEFAULT']['BOT_USER']
+
+
+def discordPost(toChannel, msg):
+    client = discord.Client()
+
+    @client.event
+    async def on_ready():
+        print('Logged in as {0.user}'.format(client))
+        channel = discord.utils.get(client.get_all_channels(), name=toChannel)
+        if channel:
+            await channel.send(msg)
+            print("Posted to %s, the message: %s" % (toChannel, msg))
+        await client.close()
+        
+    client.run(BOT_TOKEN)
+    
+if __name__ == "__main__":
+    assert len(sys.argv) == 3 and "Need channelName and message"
+    toChannel = sys.argv[1]
+    msg = sys.argv[2]
+    discordPost(toChannel, msg)
+
---
+++ discordPostRandomQuote.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import csv
+import os
+import sys
+import configparser
+import discord
+import random
+
+USER_INI = os.path.abspath(os.path.expanduser('~/.dbot'))
+
+config = configparser.ConfigParser(allow_no_value=True)
+config.read(USER_INI)
+
+BOT_TOKEN = config['DEFAULT']['BOT_TOKEN']
+BOT_USER = config['DEFAULT']['BOT_USER']
+
+QUOTES_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'quotes.tsv')
+
+# CHANNEL_NAME = 'privateadmin'
+CHANNEL_NAME = 'chit-chat'
+
+def getRandomQuote():
+    with open(QUOTES_FILE) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter='\t')
+        lineCount = 0
+        lines = []
+        # skip header
+        next(csv_reader)
+        for row in csv_reader:
+            lineCount += 1
+            lines.append(row)
+    num = random.randint(0, len(lines) - 1)
+    quote = lines[num][0]
+    print(quote)
+    return quote
+
+def discordPost():
+    msg = getRandomQuote()
+    client = discord.Client()
+
+    @client.event
+    async def on_ready():
+        channel = discord.utils.get(client.get_all_channels(), name=CHANNEL_NAME)
+        if channel:
+            await channel.send(msg)
+        await client.close()
+        
+    client.run(BOT_TOKEN)
+    
+if __name__ == "__main__":
+    discordPost()
+
